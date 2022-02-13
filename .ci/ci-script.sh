7fb1f5fe07daf9b756bda22041ebbf8bb69daf67
jakubfi <jakubfi@users.noreply.github.com>
2022-02-13 17:13:11 +0100

    Update ci-script.sh

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -6,11 +6,18 @@ case $ACTION in
 
 	init)
 		cmake -E make_directory "$BUILD_DIR"
+		echo === Building EMAWP ============================================
 		git clone https://github.com/jakubfi/emawp
 		cmake -E make_directory emawp/build
 		cmake emawp -G "Unix Makefiles" -B emawp/build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/mingw64
 		cmake --build emawp/build
 		cmake --install emawp/build
+		echo === Building EMAS ============================================
+		git clone https://github.com/jakubfi/emas
+		cmake -E make_directory emas/build
+		cmake emas -G "Unix Makefiles" -B emas/build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/mingw64
+		cmake --build emas/build
+		cmake --install emas/build
 	;;
 
 	configure)
